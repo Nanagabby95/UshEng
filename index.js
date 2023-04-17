7f65ba9e5a0d64306207d2411378286277e3e83b
Gabriel Nancy <115461625+Nanagabby95@users.noreply.github.com>
2023-04-17 16:44:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-// Get the popup form and the buttons that trigger it
-const popup = document.getElementById("divOne");
-const btns = document.querySelectorAll("#open-form, #footer-btn, #hero-btn");
 
-// When a button is clicked, show the popup form
+// SignUp button
+const btns = document.querySelectorAll("#footer-btn, #open-form, #hero-btn");
+const popup = document.getElementById('popup');
+const closeBtn = document.getElementById('close-btn');
+
+
+
 btns.forEach(btn => {
   btn.addEventListener("click", function() {
     popup.style.display = "block";
   });
 });
 
-// When the user clicks anywhere outside of the popup form, close it
-window.addEventListener("click", function(event) {
-  if (event.target == popup) {
-    popup.style.display = "none";
-  }
+closeBtn.addEventListener('click', () => {
+	popup.style.display = 'none';
+});
+
+
+const createAccountBtn = document.getElementById('create-account-btn');
+const popupForm = document.getElementById('divOne');
+const closeBttn = document.getElementById('close-icon');
+
+createAccountBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  popupForm.style.display = 'block';
+});
+
+closeBttn.addEventListener('click', () => {
+	popupForm.style.display = 'none';
+});
+
+createAccountBtn.addEventListener('click',function(){
+  popup.style.display='none';
+});
+
+const otpBtn = document.getElementById('OTP-btn');
+const OTPform = document.getElementById('OTPform');
+
+otpBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  OTPform.style.display = 'block';
+  alert('An OTP code has been sent to your registered mail');
+});
+
+otpBtn.addEventListener('click', function(){
+  divOne.style.display='none';
+});
+
+const OTPclose=document.getElementById('OTPclose');
+
+OTPclose.addEventListener('click', function(){
+  OTPform.style.display='none';
 });
